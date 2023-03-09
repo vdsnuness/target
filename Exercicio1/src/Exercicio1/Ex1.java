af972e851216ad3f36a68845edb5b98c83bc9eae
Victor Nunes <111540548+vdsnuness@users.noreply.github.com>
2023-03-09 17:53:57 -0300

    Add files via upload

---
+++ Exercicio1/src/Exercicio1/Ex1.java
@@ -0,0 +1,16 @@
+package Exercicio1;
+
+public class Ex1 {
+	public static void main(String[] args) {
+		int ind = 13, soma = 0, k = 0;
+        
+        while(k<ind){
+            k = k+1;
+            soma=soma+k;
+            
+        }
+        
+        System.out.println(soma);
+	}
+
+}
